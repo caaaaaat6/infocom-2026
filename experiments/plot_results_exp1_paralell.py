6159854ec91118947b0ba137811a741ff3cc88e1
caaaaaat <zhangyb36@mail2.sysu.edu.cn>
2025-07-21 19:00:36 +0800

    to pull

--- experiments/plot_results_exp1_paralell.py
+++ experiments/plot_results_exp1_paralell.py
@@ -36,7 +36,7 @@ def plot_single_experiment_results(experiment_name: str, base_filename: str):
     """
     print(f"--- 正在为 {experiment_name} 绘制结果图表 ---")
 
-    base_filename = "experiment_1_results_2025-07-21_17-17-26.json"
+    base_filename = "experiment_1_results_2025-07-21_03-33-27.json"
     project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     filepath = os.path.join(project_root, 'results', base_filename)
 
